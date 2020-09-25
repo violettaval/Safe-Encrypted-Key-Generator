e618ddeec5b7cb7d34231589e793b74d7e9f19e4
violettaval <70643274+violettaval@users.noreply.github.com>
2020-09-25 15:45:17 -0500

    6 hours trying to remove overload

--- script.js
+++ script.js
@@ -1,42 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// asks which stuffins
+// function loopPrompt() {
+//   for(var i = 0; i < this.length; i++) {
+//     this[i] = confirm("Would like to include: " + this[i]);
+//     //console on
+//     console.log(this[i]);
+//   }
+// }
 // laying down the blocks
-
-
+var passwordTextOptions = ["upper Case Characters", "lower Case Characters", "special Characters", "number Characters"];
+var upperCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[0]);
+var lowerCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[1]);
+var specialCharacters = confirm("Would you like to include: " + passwordTextOptions[2]);
+var numberCharacters = confirm("Would you like to include: " + passwordTextOptions[3]);
+var charLength = function () {
+  while ((charLength < 8) || (charLength > 128) || (charLength = null)) {
+    charLength = parseInt(Math.floor(prompt("Pick a secure password length from 8 to 128 character")));
+    // console on
+    console.log("your character length is " + charLength);
+  }
+  return parseInt(charLength);
+}
 var lowerChar = "qwertyuiopasdfghjklzxcvbnm";
 var upperChar = lowerChar.toUpperCase();
 var specialChar = "~!@#$%&-+=_";
+// puts in da good stuff 
+var passwordOptions = [upperCaseCharacters, lowerCaseCharacters, specialCharacters, numberCharacters];
+var passwordTextOptions = ["upper Case Characters", "lower Case Characters", "special Characters", "number Characters"];
+//looks for true in an array to check if at least one character is true 
 
-//spice jars
+//spice jar
 var specialSauce = [];
-var passwordOptions =[];
-
 
 //We make a password in here
-function hotMixr(passwordOptions) {
+
+function hotMixr() {
   // big spork holds the flavor
-  var spoon;
-  // rng
-  var coinTosser;
+  var spoon = "string";
+  // heat switch start cookin
+  var onSwitch = true;
+  var coinTosser = 5;
   do {
+    // rng internal to each stirr
     coinTosser = Math.floor(Math.random() * 4);
+    // console on
+    console.log("the coin toss was " + coinTosser);
+    //looper that 
     for (var k = 0; k < passwordOptions.length; k++) {
       // choose a random type of character from that group grab it with spoon
-      console.log(passwordOptions[k])
       if ((passwordOptions[k] === true) && (coinTosser === k)) {
         if (k === 0) {
           spoon = upperChar[Math.floor(Math.random() * upperChar.length)]
           //console on 
           console.log("Spoon caught holding " + spoon);
-          // //stops the while loop
+          //stops the while loop
+          onSwitch = false;
+          // stop the for loop incase onswitch keeps runnin
           break;
         }
         else if (k === 1) {
           spoon = lowerChar[Math.floor(Math.random() * lowerChar.length)]
           //console on 
           console.log("Spoon caught holding " + spoon);
-          // //stops the while loop
-          // onSwitch = false;
+          //stops the while loop
+          onSwitch = false;
           // stop the for loop incase onswitch keeps runnin
           break;
         }
@@ -44,8 +73,8 @@ function hotMixr(passwordOptions) {
           spoon = upperChar[Math.floor(Math.random() * upperChar.length)]
           //console on 
           console.log("Spoon caught holding " + spoon);
-          // //stops the while loop
-          // onSwitch = false;
+          //stops the while loop
+          onSwitch = false;
           // stop the for loop incase onswitch keeps runnin
           break;
         }
@@ -57,65 +86,32 @@ function hotMixr(passwordOptions) {
           // stop the for loop incase onswitch keeps runnin
           break;
         }
-      }
-      else {
-        coinTosser = Math.floor(Math.random() * 4);
+        else {
+          // try again something went wrong
+          console.log("whoops it broke");
+          break;
+        }
       }
     }
-
   }
-  while (spoon === null);
-  console.log("spoon is holding");
-  return spoon;
-}
-
-
-function validator(passwordOptions, passwordTextOptions) {
-  passwordOptions = {upperCaseCharacters, lowerCaseCharacters, specialCharacters, numberCharacters};
-
-  do {
-    alert("Must select at least one type of character.");
-    this.upperCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[0] + "?");
-    this.lowerCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[1] + "?");
-    this.specialCharacters = confirm("Would you like to include: " + passwordTextOptions[2] + "?");
-    this.numberCharacters = confirm("Would you like to include: " + passwordTextOptions[3] + "?");
-    
-    console.log(passwordOptions);
-    // good guy greg checks if at least one type of character is check and will loop options again if not  
-  }
-  while ((this.upperCaseCharacters === false) && (this.lowerCaseCharacters === false) && (this.specialCharacters === false) && (this.numberCharacters === false))
-    console.log("You made it this far valid");
-  return passwordOptions;
+  while (onSwitch === true);
+  return spoon.toString();
 }
 function generatePassword() {
-  // empty jar
-  specialSauce = [];
-  console.log("we made this far");
-  validator(passwordOptions);
-
-  //checker for char length must be between 8 and 128
-  var charLength = (function () {
-    do {
-      charLength = prompt("Pick a secure password length from 8 to 128 character");
-      // console on
-      console.log("your character length is " + charLength);
-    }
-    while ((charLength < 8) || (charLength > 128) || (charLength === null));
-    return parseInt(charLength);
-  });
   for (var j = 0; j < charLength; j++) {
+    //da cook
+    //console on
+    
     //hot kookin
-    specialSauce.push(hotMixr(passwordOptions));
+    specialSauce[j] = hotMixr();
   }
-  //console on
-  console.log("sauce is " + specialSauce);
-  return specialSauce.join("");
+  console.log("sauce is " + specialSauce.toString);
+  return specialSauce;
 }
 // Write password to the #password input
 function writePassword() {
-  passwordOptions = validator(passwordOptions);
   var password = generatePassword();
-  var passwordText = document.getElementById("#password");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
