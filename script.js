8b572af042cad9b0128f4b398ee5337e3fdca97a
violettaval <70643274+violettaval@users.noreply.github.com>
2020-09-25 15:51:52 -0500

    I split the mixer rebuilt the code again from ground up I dont know why it wont work

--- script.js
+++ script.js
@@ -1,106 +1,101 @@
- // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code
 
-// laying down the blocks
-var upperCaseCharacters = true;
-var lowerCaseCharacters = true;
-var specialCharacters = false;
-var numberCharacters = false;
-var charLength = function () {
-  while ((charLength < 8) || (charLength > 128) || (charLength = null)) {
-    charLength = parseInt(Math.floor(prompt("Pick a secure password length from 8 to 128 character")));
-    // console on
-    console.log("your character length is " + charLength);
-  }
-  return parseInt(charLength);
-}
+
+var generateBtn = document.querySelector("#generate");
 var lowerChar = "qwertyuiopasdfghjklzxcvbnm";
 var upperChar = lowerChar.toUpperCase();
 var specialChar = "~!@#$%&-+=_";
-// puts in da good stuff 
-var passwordOptions = [upperCaseCharacters, lowerCaseCharacters, specialCharacters, numberCharacters];
+var charLength = charLengthAsk();
+
+function charLengthAsk() {
+    do {
+        charLength = Math.floor(prompt("Pick a secure password length from 8 to 128 character"));
+        console.log("your character length is " + charLength);
+    }
+    while (((charLength < 8) || (charLength > 128) || (charLength = null)));
+    return parseInt(charLength);
+}
+
 var passwordTextOptions = ["upper Case Characters", "lower Case Characters", "special Characters", "number Characters"];
-//looks for true in an array to check if at least one character is true 
+var passwordOptions = validator(passwordTextOptions);
+
+function validator(passwordTextOptions) {
+    do {
+        alert("At Least One type of Characters must be chosen");
+        var upperCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[0]);
+        var lowerCaseCharacters = confirm("Would you like to include: " + passwordTextOptions[1]);
+        var specialCharacters = confirm("Would you like to include: " + passwordTextOptions[2]);
+        var numberCharacters = confirm("Would you like to include: " + passwordTextOptions[3]);
+        // good guy greg checks if at least one type of character is check and will loop options again if not  
+    }
+    while ((upperCaseCharacters === false) && (lowerCaseCharacters === false) && (specialCharacters === false) && (numberCharacters == false));
+    return [upperCaseCharacters, lowerCaseCharacters, specialCharacters, numberCharacters];
+}
+
+console.log(passwordOptions);
 //spice jar
 var specialSauce = [];
 
-//We make a password in here
+function rando(k) {
+    var text;
+    if (k === 0) {
+        text = upperChar.charAt(Math.floor(Math.random() * upperChar.length));
+        return text;
+    }
+    if (k === 1) {
+        text = lowerChar.charAt(Math.floor(Math.random() * lowerChar.length));
+        return text;
+    }
+    if (k === 2) {
+        text = specialChar.charAt(Math.floor(Math.random() * specialChar.length));
+        return toString(text);
+    }
+    if (k === 3) {
+        text = charAt(Math.floor(Math.random() * 10));
+        return toString(text);
+    }
+    else {
+        return null;
+    }
+}
 
-function hotMixr(passwordOptions) {
-  // big spork holds the flavor
-  var spoon;
-  // heat switch start cookin
-  var onSwitch = true;
-  var coinTosser = 5;
-  while (onSwitch === true) {
-    // rng internal to each stirr
-    coinTosser = Math.floor(Math.random() * 4);
-    // console on
-    console.log("the coin toss was " + coinTosser);
-    //looper that 
-    numbers.forEach(function(){
-      for (var k = 0; k < passwordOptions.length; k++) {
-        // choose a random type of character from that group grab it with spoon
-        if ((passwordOptions[k] === true) && (coinTosser === k)) {
-          if (k === 0) {
-            spoon = upperChar[Math.floor(Math.random() * upperChar.length)]
-            //console on 
-            console.log("Spoon caught holding " + spoon);
-            //stops the while loop
-            onSwitch = false;
-            // stop the for loop incase onswitch keeps runnin
-            break;
-          }
-          else if (k === 1) {
-            spoon = lowerChar[Math.floor(Math.random() * lowerChar.length)]
-            //console on 
-            console.log("Spoon caught holding " + spoon);
-            //stops the while loop
-            onSwitch = false;
-            // stop the for loop incase onswitch keeps runnin
-            break;
-          }
-          else if (k === 2) {
-            spoon = upperChar[Math.floor(Math.random() * upperChar.length)]
-            //console on 
-            console.log("Spoon caught holding " + spoon);
-            //stops the while loop
-            onSwitch = false;
-            // stop the for loop incase onswitch keeps runnin
-            break;
-          }
-          else{
-            spoon = Math.floor(Math.random() * 10);
-            console.log("Spoon caught holding " + spoon);
-            //stops the while loop
-            onSwitch = false;
-            // stop the for loop incase onswitch keeps runnin
-            break;
-          }
+//We make a password in here
+function hotMixr() {
+    // big spork holds the flavor
+    var spoon = null;
+    // heat switch start cook
+    var coinTosser;
+    while (spoon === null) {
+        // rng internal to each stirr
+        coinTosser = Math.floor(Math.random() * 4);
+        // console on
+        console.log("the coin toss was " + coinTosser);
+        //looper that 
+        for (var k = 0; k < passwordOptions.length; k++) {
+            // choose a random type of character from that group grab it with spoon
+            if ((passwordOptions[k] === true) && (coinTosser === k)) {
+                spoon = rando(k);
+            }
         }
-        return spoon;
-    },k, passwordOptions;
+    }
+    return spoon;
 }
+
 function generatePassword() {
-  // empty jar
-  specialSauce=[];
-  // console.log("we made this far");
-  for (var j = 0; j < charLength; j++) {
-    //da cook
-    //hot kookin
-    specialSauce.push(hotMixr(charLength,passwordOptions));
-    // specialSauce[j] = hotMixr(charLength,passwordOptions);
-  }
-  //console on
-  console.log("sauce is " + specialSauce);
-  return specialSauce.join("");
+    for (var j = 0; j < charLength; j++) {
+        //da cook
+        specialSauce.push(hotMixr());
+    }
+    console.log("sauce is " + specialSauce);
+    return specialSauce.join("");
 }
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
